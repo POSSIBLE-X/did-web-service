eb630984a4ccf21b1a8551557bd5fd58ba322ed8
M-Busk <125284318+M-Busk@users.noreply.github.com>
2025-01-29 15:11:38 +0100

    another annotation

--- src/main/java/eu/possiblex/didwebservice/controller/InternalController.java
+++ src/main/java/eu/possiblex/didwebservice/controller/InternalController.java
@@ -4,12 +4,13 @@ import eu.possiblex.didwebservice.models.dto.ParticipantDidCreateRequestTo;
 import eu.possiblex.didwebservice.models.dto.ParticipantDidTo;
 import eu.possiblex.didwebservice.models.dto.ParticipantDidUpdateRequestTo;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/internal")
 public interface InternalController {
     @Operation(summary = "Generate a new did:web identity", tags = {
-        "Management" }, description = "Generate a new did:web identity based on the data in the payload")
+        "Management" }, description = "Generate a new did:web identity based on the data in the payload. Only the subject is mandatory.")
     @PostMapping("/didweb")
     ParticipantDidTo generateDidWeb(@RequestBody ParticipantDidCreateRequestTo to);
 
@@ -19,7 +20,8 @@ public interface InternalController {
     void updateDidWeb(@RequestBody ParticipantDidUpdateRequestTo to);
 
     @Operation(summary = "Delete a did:web identity", tags = {
-        "Management" }, description = "Delete the provided did:web identity that is managed by this did-web-service")
+        "Management" }, description = "Delete the provided did:web identity that is managed by this did-web-service", parameters = {
+        @Parameter(name = "did", description = "The did:web managed by this Service to be deleted", example = "did:web:example.com:participant:someorgltd") })
     @DeleteMapping("/didweb/{did}")
     void removeDidWeb(@PathVariable String did);
 }
